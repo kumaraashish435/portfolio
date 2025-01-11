81b8f772470491c3d88c8081934ff5a5378bb908
kumar aashish <kumaraashish56712@gmail.com>
2025-01-11 07:59:44 -0800

    Updated project with new changes

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css'; // Import the CSS file
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li className="nav-item">
+          <Link to="/" className="nav-link">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/about" className="nav-link">About</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/projects" className="nav-link">Projects</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/contact" className="nav-link">Contact</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
