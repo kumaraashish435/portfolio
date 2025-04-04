b6b95a3fbd255f978d3d196af0004c815a625496
kumar <kumaraashishcr435@gmail.com>
2025-04-04 23:25:06 +0530

    changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
+// eslint-disable-next-line no-unused-vars
 import { motion } from "framer-motion";
 
 const Navbar = () => {
